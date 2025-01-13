a08db5df74e0099f34f9d061858ee7816351908a
Jose Arturo Moreno Aguilar <JMoreno@PC-Soporte1.acr.com.mx>
2025-01-13 12:57:48 -0600

    Se hace nuevamente carpetas dañadas

--- newUsuario.php
+++ newUsuario.php
@@ -3,6 +3,8 @@
 $password = "inicio0125"; //$_POST['password'];
 // Hasheamos la contraseña con un algoritmo seguro (por defecto, PASSWORD_BCRYPT)
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-$query = "INSERT INTO USUARIO VALUE(DEFAULT,'AMora','Abel de Jesus','Mora','Flores','ADM_SISTEMAS','SISTEMAS','$hashed_password')";
+/*$query = "INSERT INTO USUARIO VALUE(DEFAULT,'AMora','Abel de Jesus','Mora','Flores','ADM_SISTEMAS','SISTEMAS','$hashed_password')";
+mysqli_query($con, $query);
+$query= "UPDATE usuario SET contrasena = '$hashed_password' WHERE id_usuario = 2";
 mysqli_query($con, $query);*/
 ?>
