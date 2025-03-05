134c40573df9ce88ec07d1d7cbb3e84b5106bdf2
Jose Arturo Moreno Aguilar <JMoreno@PC-AuxSistemas.acr.com.mx>
2025-03-05 17:47:38 -0600

    se creo pagina de error

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $dbHost='127.0.0.1';
+    /*$dbHost='127.0.0.1';
     $dbName='bd_acrivera';
     $dbUser='root';
     $dbPass="";
@@ -12,5 +12,21 @@
     }catch(Exception $ex){
         //echo $ex=getMessage();
         echo 'error';
+    }*/
+    mysqli_report(MYSQLI_REPORT_OFF); // Desactiva los errores fatales de MySQLi
+
+    $dbHost = '127.0.0.1';
+    $dbName = 'bd_acrivera';
+    $dbUser = 'root';
+    $dbPass = "";
+
+    $con = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+
+    if (!$con) {
+        echo "<script>alert('Error: No se pudo conectar a la base de datos');</script>";
+        header('Location: error.html');
+        //return false; // Devuelve false si no se conecta
     }
+
+    return $con; // Retorna la conexión si es exitosa*/
 ?>
