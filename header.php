c5ac615fe12e546e07f3549ebfd46a0f2b75aa49
Jose Arturo Moreno Aguilar <JMoreno@PC-AuxSistemas.acr.com.mx>
2025-06-09 16:11:54 -0600

    se agrega link para fontawesome para icono de whatsapp

--- header.php
+++ header.php
@@ -24,6 +24,7 @@ if (!isset($_SESSION['ususario'])) {
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <!--Manifiest-->
     <link rel="manifest" href="json/app.webmanifest">
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <!--++++++++++-->
     <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">-->
 
